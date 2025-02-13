dc8c0569dcac6ed5386bbef3eb75770a51204eb4
Nastya <anastasion0107@gmail.com>
2025-02-13 22:01:05 +0700

    tests

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -6,8 +6,31 @@ class CollectTest extends TestCase
 {
     public function testCount()
     {
-        $collect = new Collect\Collect([13,17]);
+        $collect = new Collect\Collect([13, 17]);
         $this->assertSame(2, $collect->count());
     }
 
+    public function testPush()
+    {
+        $collect = new Collect\Collect([1, 2]);
+        $collect->push(3);
+        $this->assertEquals(new Collect\Collect([1, 2, 3]), $collect);
+
+        $collect->push(4, 'b');
+        $this->assertEquals(new Collect\Collect([1, 2, 3, 'b' => 4]), $collect);
+    }
+
+    public function testUnshift()
+    {
+        $collect = new Collect\Collect([2, 3]);
+        $collect->unshift(1);
+        $this->assertEquals(new Collect\Collect([1, 2, 3]), $collect);
+    }
+
+    public function testShift()
+    {
+        $collect = new Collect\Collect([1, 2, 3]);
+        $collect->shift();
+        $this->assertEquals(new Collect\Collect([2, 3]), $collect);
+    }
 }
